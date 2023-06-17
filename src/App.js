30985d970803c9859bf1b81162d29fb30362c28a
Jartius <JStavey13@users.noreply.github.com>
2023-06-17 16:05:04 -0400

    added a nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import MyReviews from './components/MyReviews';
+import Nav from './components/Nav'
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <MyReviews />
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css'
+
+
+const Nav = () => {
+    return (
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"></img>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+    )
+
+
+
+}
+
+
+
+
+
+export default Nav;
