dbebe7c9c7c0420f03058c760d2d15c85a370a44
Jartius <JStavey13@users.noreply.github.com>
2023-06-22 16:12:36 -0400

    modified links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import MyReviews from './components/MyReviews';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import Nav from './components/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm/>
-      <MyReviews/>
+      
+      <Nav />
     </div>
   );
 }
