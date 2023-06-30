fde2699def90298e419f5a7427350460434b258f
Jartius <JStavey13@users.noreply.github.com>
2023-06-30 14:37:33 -0400

    changed background and added explore file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import MyReviews from './components/MyReviews';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 import Nav from './components/Nav'
+import Explore from './components/Explore';
 import './App.css';
 
 function App() {
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,11 +17,11 @@ const links = [
     return (
       <div className="flex items-center justify-center relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
         <img
-          src="https://images.pexels.com/photos/248812/pexels-photo-248812.jpeg?auto=compress&cs=tinysrgb&w=600"
+          src="https://images.pexels.com/photos/1353938/pexels-photo-1353938.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
         />
-        <div
+        {/* <div
           className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
           aria-hidden="true"
         >
@@ -32,8 +32,8 @@ const links = [
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
           />
-        </div>
-        <div
+        </div> */}
+        {/* <div
           className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
           aria-hidden="true"
         >
@@ -44,14 +44,14 @@ const links = [
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
           />
-        </div>
+        </div> */}
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-4xl font-bold tracking-tight title text-white sm:text-6xl">PandaCritic🐼</h2>
+            <h2 className="text-4xl font-bold tracking-tight title text-black sm:text-6xl">PandaCritic🐼</h2>
             
           </div>
           <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-black sm:grid-cols-2 md:flex lg:gap-x-10">
               {links.map((link) => (
                 <a key={link.name} href={link.href}>
                   {link.name} <span aria-hidden="true">&rarr;</span>
