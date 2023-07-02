a12c32dba4e26bd97dd805c34536a64c2a09ae5d
tanglero4 <tiffanyanglero94@hotmail.com>
2023-07-02 13:29:29 -0400

    Added location function

---
+++ src/components/restaurant.js
@@ -0,0 +1,25 @@
+let map;
+
+function newLocation(city) {
+  if (city) {
+    //Get the lat and Long
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${city}do&key=AIzaSyCOYW44XORsf-nBZKXvYwZ8VPxDIgq8X7w`,
+      {
+        method: "GET",
+        dataType: "jsonp",
+        headers: {},
+      }
+    )
+      .then((response) => response.json())
+      .then((city) => {
+        let lat = city.results[0].geometry.location.lat;
+        let long = city.results[0].geometry.location.lng;
+
+        // Send coordinates to google API 
+        googleMap(lat, long);
+        getTea(lat, long);
+      });
+  }
+}
+
