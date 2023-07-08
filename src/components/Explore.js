bcaf16423f87d2b09fe6b43a8585fbacfa5e6c6b
Jartius <JStavey13@users.noreply.github.com>
2023-07-08 09:58:37 -0400

    formatting explore page

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -16,9 +16,9 @@ return (
 <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+          <h1 className=" text-4xl sm:text-4xl font-bold">Deploy faster</h1>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Everything you need to deploy your app
+            Explore!
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
