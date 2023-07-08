43a2be50e895969f1ce3f561171962ec88c41328
Jartius <JStavey13@users.noreply.github.com>
2023-07-08 10:30:41 -0400

    working on getting pics to display

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Nav from "./Nav"
-
+import { useHref } from "react-router-dom"
+import Restaurant from "../image/restaurant.webp"
 const features = [
   {
     name: 'Boba shops',
-    image: ''
+    image: 'Restaurant'
   }
 ]
 
@@ -14,32 +15,28 @@ return (
 < Nav />
 
 <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:text-center">
-          <h1 className=" text-4xl sm:text-4xl font-bold">Deploy faster</h1>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Explore!
-          </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-            pulvinar et feugiat blandit at. In mi viverra elit nunc.
-          </p>
-        </div>
-        {/* <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+<div className="mx-auto max-w-7xl px-6 lg:px-8">
+  <div className="mx-auto max-w-2xl lg:text-center">
+    <h1 className="text-4xl sm:text-4xl font-bold">Deploy faster</h1>
+    
+  </div>
+
+
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-gray-900">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                    <feature.image className="h-6 w-6 text-white" aria-hidden="true" />
                   </div>
                   {feature.name}
-                </dt> */}
+                </dt>
                 {/* <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd> */}
-              {/* </div>
+               </div>
             ))}
           </dl>
-        </div> */}
+        </div> 
       </div>
     </div>
 
