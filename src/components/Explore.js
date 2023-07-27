60d7ee2e92f51cce51b89a948394148130d24fc6
Jartius <JStavey13@users.noreply.github.com>
2023-07-27 15:52:52 -0400

    resized explore pics and added explore banner

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -27,14 +27,17 @@ export default function Explore() {
   return (
     <div>
       <Nav />
-      <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+      {/* <div className="bg-white py-24 sm:py-32"> */}
+        <div id="explore-banner">
+          Explore!
+        </div>
+
+          <div id="explore-box">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 justify-center lg:max-w-none lg:grid-cols-3 lg:gap-y-16">
               {features.map((feature) => (
                 <div key={feature.name}  className="relative">
                   <div className="h-40 w-90vw sm:w-80 mx-auto flex items-center justify-center rounded-lg bg-indigo-600">
-                    <img src={feature.image} alt="Bootcamp" className="max-h-full max-w-full text-white" />
+                    <img src={feature.image} alt="Bootcamp" className="max-h-full max-w-full text-white image" />
                   </div>
                   <dt className="text-base font-semibold leading-7 text-gray-900 text-center mt-2" >
                   <a href={feature.link}>{feature.name}</a>
@@ -43,8 +46,8 @@ export default function Explore() {
               ))}
             </dl>
           </div>
-        </div>
-      </div>
-    </div>
+
+      {/* // </div> */}
+    // </div>
   );
 }
